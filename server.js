98df6164c7ff04f62e9f864fa2d0e2e60c09da42
Harsh <harshkumarpandit2004@gmail.com>
2026-02-14 00:30:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"]
+    origin: ["http://localhost:5173", "http://localhost:5174","https://e-commerce-frontend-five-khaki.vercel.app
+]
   })
 );
 
